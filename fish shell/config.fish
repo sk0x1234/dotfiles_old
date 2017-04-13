8374523551c91bbb13881b1f2cf316afc61dda17
Naruto Windy <narutowindy@users.noreply.github.com>
2017-04-13 15:41:48 +0530

    Add files via upload

---
+++ fish shell/config.fish
@@ -0,0 +1,11 @@
+function spp
+	cd /mnt/driveE/Dump_linux/programming/PyProgmng
+end
+
+
+function scp
+	cd /mnt/driveE/Dump_linux/programming/C_tutorials
+end
+function torrent
+ 	cd /mnt/driveE/Random_Downloads/Torrents
+ end
